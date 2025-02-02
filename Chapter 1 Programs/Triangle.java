162012d31817a8b7dfe656506a2f47d9d5753df6
Kiara Bilyk <160333274+Kiara-Bilyk@users.noreply.github.com>
2025-02-02 14:52:53 -0500

    Update Triangle.java

--- Chapter 1 Programs/Triangle.java
+++ Chapter 1 Programs/Triangle.java
@@ -1,3 +1,13 @@
+/*    Java Programming 10th Edition
+      Chapter 1
+      Project 01-09
+
+      Class that displays an image of a triangle using 'T'.
+      Author: Kiara Bilyk
+      Date:   01-30-2025
+
+      Filename: Triangle.java
+*/
 public class Triangle {
     public static void main(String[] args) {
         System.out.println("      T");
