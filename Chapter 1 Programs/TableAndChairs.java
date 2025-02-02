15fe24001657a9bf80bf1b2975893a2467193475
Kiara Bilyk <160333274+Kiara-Bilyk@users.noreply.github.com>
2025-02-02 14:51:57 -0500

    Update TableAndChairs.java

--- Chapter 1 Programs/TableAndChairs.java
+++ Chapter 1 Programs/TableAndChairs.java
@@ -1,3 +1,13 @@
+/*    Java Programming 10th Edition
+      Chapter 1
+      Project 01-08
+
+      Class that displays an image of a table and two chairs using 'X'.
+      Author: Kiara Bilyk
+      Date:   01-30-2025
+
+      Filename: TableAndChairs.java
+*/
 public class TableAndChairs {
     public static void main(String[] args) {
         System.out.println("X                    X");
