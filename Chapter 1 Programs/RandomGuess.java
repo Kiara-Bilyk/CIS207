76ceb0e2ce5b70aaf87708b6c3b52c8abca3609e
Kiara Bilyk <160333274+Kiara-Bilyk@users.noreply.github.com>
2025-02-06 14:45:00 -0500

    Update RandomGuess.java

--- Chapter 1 Programs/RandomGuess.java
+++ Chapter 1 Programs/RandomGuess.java
@@ -1,4 +1,15 @@
+/*    Java Programming 10th Edition
+      Chapter 1
+      GameZone 01-01
 
+      Java application that displays two dialog boxes in sequence. 
+      The first asks you to think of a number between 1 and 10. 
+      The second displays a randomly generated number
+      Author: Kiara Bilyk
+      Date:   02-05-2025
+
+      Filename: RandomGuess.java
+*/
 import javax.swing.JOptionPane;
 
 public class RandomGuess {
