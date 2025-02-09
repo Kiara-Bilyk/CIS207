8d61a9a937b4db4de14f91495bcad1f237adfcd6
Kiara Bilyk <160333274+Kiara-Bilyk@users.noreply.github.com>
2025-02-09 17:45:12 -0500

    Add files via upload

---
+++ Chapter 1 Programs/SunshineMotto.java
@@ -0,0 +1,16 @@
+/*    Java Programming 10th Edition
+      Chapter 1
+      Case Problem 01-02a
+
+       Write a program that displays Sunshine’s motto, which is "Sunshine Seashore makes it fun in the sun".
+
+      Author: Kiara Bilyk
+      Date:   02-05-2025
+
+      Filename: SunshineMotto.java
+*/
+public class SunshineMotto {
+    public static void main(String[] args) {
+        System.out.println("Sunshine Seashore makes it fun in the sun");
+    }
+}
---
+++ Chapter 1 Programs/SunshineMotto2.java
@@ -0,0 +1,18 @@
+/*    Java Programming 10th Edition
+      Chapter 1
+      Case Problem 01-01b
+
+      Create a second program that displays the motto surrounded by a border composed of repeated "S"s.
+
+      Author: Kiara Bilyk
+      Date:   02-05-2025
+
+      Filename: SunshineMotto2.java
+*/
+public class SunshineMotto2 {
+    public static void main(String[] args) {
+        System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+        System.out.println("S Sunshine Seashore makes it fun in the sun S");
+        System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+    }
+}
