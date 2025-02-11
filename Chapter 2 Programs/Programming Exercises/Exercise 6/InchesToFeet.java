f110ed36b0dc2e279d04b135620fe89cf88475b1
Kiara Bilyk <160333274+Kiara-Bilyk@users.noreply.github.com>
2025-02-10 19:26:54 -0500

    Add files via upload

---
+++ Chapter 2 Programs/Programming Exercises/Exercise 6/InchesToFeet.java
@@ -0,0 +1,19 @@
+/*    Java Programming 10th Edition
+      Chapter 1
+      Exercises 02-06a
+
+       Write a program that declares a variable named inches, which holds a length in inches, and assign a value. 
+       Display the value in feet and inches.
+
+      Author: Kiara Bilyk
+      Date:   02-07-2025
+
+      Filename: InchesToFeet.java
+*/
+public class InchesToFeet {
+    public static void main(String[] args) {
+        final int INCH_IN_FEET = 12;
+        int inches = 86;
+        System.out.println(inches + " inches equals " + (inches/INCH_IN_FEET) + " feet and " + (inches%INCH_IN_FEET) + " inches.");
+    }
+}
